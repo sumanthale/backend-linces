3be2a973a441a362f5e6ae5b34497f6f66ba1aa9
Ale Sumanth <sumanthale@gmail.com>
2026-04-17 01:58:56 +0530

    clean sh

--- scripts/prisma-clean.sh
+++ scripts/prisma-clean.sh
@@ -2,9 +2,6 @@
 
 echo "🧹 Cleaning Prisma setup..."
 
-# Remove generated prisma client
-rm -rf node_modules/.prisma
-
 # Remove prisma migrations
 rm -rf prisma/migrations
 
@@ -13,8 +10,7 @@ rm -f prisma/dev.db
 rm -f dev.db
 rm -f *.db
 
-# Remove prisma client build
-rm -rf node_modules/@prisma/client
+
 
 echo "✅ Prisma cleanup complete"
 echo "You can now re-run migrations.  "
